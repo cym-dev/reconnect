0f3697708849a61b06bae65e3febd3ab4e0af13f
TNWCYM <technowiz.channeyydreo.marzan@gmail.com>
2024-02-12 22:48:23 +0800

    updated

--- techno/js/index.js
+++ techno/js/index.js
@@ -28,3 +28,19 @@ $(window).on("load", function () {
   $(".loader").fadeOut();
   $("#preloder").delay(200).fadeOut("slow");
 });
+
+//effect
+const inViewport = (entries, observer) => {
+  entries.forEach(entry => {
+    entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+  });
+};
+
+const Obs = new IntersectionObserver(inViewport);
+const obsOptions = {};
+
+//Attach observer to every [data-inViewport] element
+const ELs_inViewport = document.querySelectorAll("[data-inviewport]");
+ELs_inViewport.forEach(EL => {
+  Obs.observe(EL, obsOptions);
+});
